70d76ead3791cf95086791cfa7969487d37966f3
Gabrieljes <dustty@pop-os.localdomain>
2023-02-19 13:36:00 -0300

    fix

--- apps.sh
+++ apps.sh
@@ -12,6 +12,8 @@ echo
 sudo snap install discord
 echo
 verify_discord
+sleep 1
+
 
 # Installer e verify Steam
 echo 'Installing Steam'
@@ -19,6 +21,7 @@ app='steam'
 loading 
 sudo apt install steam 
 verify_steam
+sleep 1
 
 # Installer e verify Google-chrome 
 echo 'Installing google-chrome'
@@ -26,30 +29,40 @@ app='google-chrome'
 loading
 google_installer
 verify_google_chrome
+sleep 1
+
 
 # Installer e verify vscode
 echo 'Installing vscode'
 app='code'
 loading 
 verify_code
+sleep 1
+
 
 # Installer e verify Spotify
 echo 'Installing Spotify' 
 app='spotify'
 loading 
 verify_spotify
+sleep 1
+
 
 # Installer e verify Pycharm
 echo 'Installing Pycharm' 
 app='pycharm'
 loading 
 verify_pycharm
+sleep 1
+
 
 # Installer e verify Jupyter
 echo 'Installing Jupyter' 
 app='jupyter'
 loading 
 verify_jupyter
+sleep 1
+
 
 
 
