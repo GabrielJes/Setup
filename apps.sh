55a14f890e2d842ad7cb90256c74b365e5e97fc6
Gabrieljes <dustty@pop-os.localdomain>
2023-02-18 21:40:11 -0300

    correcao

--- apps.sh
+++ apps.sh
@@ -8,47 +8,54 @@ sleep 1
 echo 'Verificando discord'
 app='discord'
 sleep 1
-loading verify_discord
+loading 
+verify_discord
 
 # Installer e verify Steam
 echo 'Verificando Steam'
 app='steam'
 sleep 1
-loading verify_steam
+loading 
+verify_steam
 
 # Installer e verify Google-chrome 
 echo 'Verificando google-chrome'
 app='google-chrome'
 sleep 1
-loading  verify_google_chorme
+loading  
+verify_google_chorme
 sleep 1
 
 # Installer e verify vscode
 echo 'Verificando vscode'
 app='code'
 sleep 1
-loading verify_code
+loading 
+verify_code
 sleep 1
 
 # Installer e verify Spotify
 echo 'Verificando Spotify' 
 app='spotify'
 sleep 1
-loading verify_spotify
+loading 
+verify_spotify
 sleep 1
 
 # Installer e verify Pycharm
 echo 'Verificando Pycharm' 
 app='pycharm'
 sleep 1
-loading verify_pycharm
+loading 
+verify_pycharm
 sleep 1
 
 # Installer e verify Jupyter
 echo 'Verificando Jupyter' 
 app='jupyter'
 sleep 1
-loading verify_jupyter
+loading 
+verify_jupyter
 
 
 
