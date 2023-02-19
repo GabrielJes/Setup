c8ed10c7494e015ca58e00e3f239fdc107ff7252
Gabrieljes <dustty@pop-os.localdomain>
2023-02-19 10:12:56 -0300

    adc

--- exec_dep.sh
+++ exec_dep.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+
+# Dependencias 
 sudo apt install neofetch -y
 source ./funcions.sh && sudo apt install figlet -y 
 figlet "Bem-vindo"
-figlet "ao BMS!"
 
 # loop s/n 
-
 sim_ou_nao
+
+# Import
 source ./list.sh
 source ./package_OS.sh
 
