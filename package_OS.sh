ee95619e296666273cd1cff3a70c9f7c93fa2be0
Gabrieljes <dustty@pop-os.localdomain>
2023-02-18 22:52:07 -0300

    Versao1

--- package_OS.sh
+++ package_OS.sh
@@ -11,24 +11,21 @@ echo
 # Net-tools installer
 app="net-tools"
 echo "Instalando net-tools"
-loading
-sudo apt install $app 
+loading sudo apt install $app 
 sleep 1  
 verify_net_tools $app
 
 # plocate installer 
 app="plocate"
 echo "Instalando plocate" 
-loading
-sudo apt install plocate 
+loading sudo apt install plocate 
 sleep 1
 verify_plocate $app
 
 # vim installer 
 app="vim"
 echo "Instalando vim"
-loading
-sudo apt install vim 
+loading sudo apt install vim 
 sleep 1
 verify_vim $app
 
@@ -36,8 +33,7 @@ verify_vim $app
 # git installer 
 app="git"
 echo "Instalando git"
-loading 
-sudo apt install git 
+loading sudo apt install git 
 sleep 1
 verify_git $app
 
@@ -45,8 +41,7 @@ verify_git $app
 # snap installer 
 app="snap"
 echo "Instalando snap"
-loading 
-sudo apt install snap 
+loading sudo apt install snap 
 sleep 1
 verify_snap $app
 
@@ -54,8 +49,7 @@ verify_snap $app
 # wget installer 
 app="wget"
 echo "Instalando wget"
-loading 
-sudo apt install wget 
+loading sudo apt install wget 
 sleep 1
 verify_wget $app
 # finished installing || 
@@ -65,8 +59,7 @@ sleep 1
 echo ' [ Atualizando os pacotes do sistema ] '
 sleep 1
 echo
-loading 
-sudo apt upgrade -y
+loading sudo apt upgrade -y
 sleep 1
 
 # versao do sistema operacional atual
