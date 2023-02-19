534e173264003e991658f31dc6190aff3b44adb2
Gabrieljes <dustty@pop-os.localdomain>
2023-02-18 21:30:08 -0300

    correcao

--- package_OS.sh
+++ package_OS.sh
@@ -17,46 +17,41 @@ sleep 1 && verify_net_tools $app
 # plocate installer 
 app="plocate"
 echo "Instalando plocate" 
-barra_de_loading 
-sudo apt install plocate -y
+loading sudo apt install plocate -y
 sleep 1 && verify_plocate $app
 
 # vim installer 
 app="vim"
 echo "Instalando vim"
-barra_de_loading 
-sudo apt install vim -y
+loading sudo apt install vim -y
 sleep 1 && verify_vim $app
 
 
 # git installer 
 app="git"
 echo "Instalando git"
-barra_de_loading 
-sudo apt install git -y
+loading sudo apt install git -y
 sleep 1 && verify_git $app
 
 
 # snap installer 
 app="snap"
 echo "Instalando snap"
-barra_de_loading  
-sudo apt install snap -y 
+loading sudo apt install snap -y 
 sleep 1 && verify_snap $app
 
 
 # wget installer 
 app="wget"
 echo "Instalando wget"
-barra_de_loading 
-sudo apt install wget -y
+loading sudo apt install wget -y
 sleep 1 && verify_wget $app
 # finished installing || 
 
 # Atualizando pacotes
 sleep 1
 echo ' [ Atualizando os pacotes do sistema ] ' && sleep 1
-barra_de_loading sudo apt upgrade -y
+loading sudo apt upgrade -y
 sleep 1
 
 # versao do sistema operacional atual
