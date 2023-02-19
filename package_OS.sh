5d5eecb33938d4b47fae4565802442b3f3ad1866
Gabrieljes <dustty@pop-os.localdomain>
2023-02-19 11:58:59 -0300

    fix

--- package_OS.sh
+++ package_OS.sh
@@ -5,37 +5,33 @@ source ./funcions_package.sh
 
 # Start installer ||
 # Net-tools installer="net-tools"
-echo " Instalando net-tools"
+echo " Installing net-tools"
 loading  
 verify_net_tools
 
 # plocate installer ="locate"
-echo "instalando locate"
+echo "Installing locate"
 loading  
 verify_locate_install $app
 
 # vim installer ="vim"
-echo "Instalando vim"
+echo "Installing vim"
 loading 
-sudo apt install vim 
 verify_vim $app
 
 
 # git installer ="git"
-echo "Instalando git"
-loading sudo apt install git 
+echo "Installing git"
 verify_git $app
 
 
 # snap installer 
-echo "Instalando snap"
-loading sudo apt install snap 
+echo "Installing snap"
 verify_snap $app
 
 
 # wget installer 
-echo "Instalando wget"
-loading sudo apt install wget 
+echo "Installing wget"
 verify_wget $app
 # finished installing || 
 
