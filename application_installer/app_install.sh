1bb95df041276217f2e81c0594eaf2a19fa6ba02
Gabrieljes <dustty@pop-os.localdomain>
2023-02-20 21:49:48 -0300

    version0

--- application_installer/app_install.sh
+++ application_installer/app_install.sh
@@ -5,31 +5,45 @@ source ./functions_apt.sh
 
 
 # Install apps and verify
-echo " Installing vscode ! "
+echo " Installing Visual Code ! "
 name_package=code
 verify_install_apps_snap $name_package
+sleep 1
 
 # Install apps and verify
+echo " Installing Steam ! "
 name_package=steam
 verify_install_apps_snap $name_package
+sleep 1
 
 # Install apps and verify
+echo " Installing Discord ! "
 name_package=discord
 verify_install_apps_apt $name_package
+sleep 1
 
 # Install apps and verify
+echo " Installing Spotify ! "
 name_package=spotify
 verify_install_apps_snap $name_package
+sleep 1
 
 # Install apps and verify
+echo " Installing Pycharm ! "
 name_package=pycharm
 verify_install_pycharm $name_package
+sleep 1
 
 # Install apps and verify
+echo " Installing Jupyter ! "
 name_package=jupyter
 verify_install_apps_apt $name_package
+sleep 1
 
 # Install apps and verify
+echo " Installing Google-Chrome ! "
 name_package=google.chrome
 verify_install_google $name_package
+sleep 1
+
 
