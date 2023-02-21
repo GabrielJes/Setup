b1b3893f1cf2758da17e3251297a3a15f18a5837
Gabrieljes <dustty@pop-os.localdomain>
2023-02-20 22:04:22 -0300

    version0

--- application_installer/functions/functions_apt.sh
+++ application_installer/functions/functions_apt.sh
@@ -29,7 +29,7 @@ fi
 
 verify_install_apps_snap() {
     name=$name_package
-    pacote=$(dpkg --get-selections | sudo find /usr -name $name)
+    pacote=$(dpkg --get-selections | sudo find $fix -name $name)
   if [ -n "$pacote" ] ;
     then 
         its_installed='true'
