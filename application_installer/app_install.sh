6f0ec53236d96e2281faa01591e688f9d684f33f
Gabrieljes <dustty@pop-os.localdomain>
2023-02-25 20:36:00 -0300

    final

--- application_installer/app_install.sh
+++ application_installer/app_install.sh
@@ -22,7 +22,7 @@ sleep 1
 
 # Install apps and verify
 echo "Installing Discord ! "
-fix='/snap/bin'
+fix='/usr/bin'
 search=discord
 name_package=discord
 verify_install_apps_snap $name_package
@@ -59,5 +59,6 @@ search=google-chrome
 name_package=google.chrome
 verify_install_google $name_package
 sleep 1
+
 
 
